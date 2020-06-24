4bbb2fa9062d7f30494587796b0192f1874b7cf1
arahansa <arahansa@naver.com>
2020-06-24 21:07:29 +0900

    110p - ForceUpdateExample

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 
 import StateExample from "./03/StateExample";
+import ForceUpdateExample from "./03/ForceUpdateExample";
 
 
 class App extends Component {
     render() {
         return (
             <div>
-                <StateExample/>
+                <ForceUpdateExample/>
             </div>
         );
     }
