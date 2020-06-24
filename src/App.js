d0e7c9c4c43da7b05620858264620f107a3d2eba
arahansa <arahansa@naver.com>
2020-06-24 15:38:13 +0900

    95 페이지 까지 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
 import TodaysPlan from "./03/TodaysPlan";
 import PropsComponent from "./03/PropsComponent";
+import ChildComponent from "./03/ChildComponent";
 
 class App extends Component {
     render() {
         return (
             <div>
-                <PropsComponent name="두잇 리액트"/>
+                <ChildComponent
+                    boolValue={true}
+                    numValue={1}
+                    arrayValue={[1,2,3]}
+                    objValue={{name:'제목', age: 30}}
+                    nodeValue={<h1>노드</h1>}
+                    funcValue={() => console.log('메시지')}
+                >
+                </ChildComponent>
             </div>
         );
     }
