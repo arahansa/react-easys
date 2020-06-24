8b2d00e6a9a107fb21563d9bc0fd02b10ab3d08d
arahansa <arahansa@naver.com>
2020-06-24 15:59:53 +0900

    102 - 기본값 지정 전

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, {Component} from 'react';
-import TodaysPlan from "./03/TodaysPlan";
-import PropsComponent from "./03/PropsComponent";
-import ChildComponent from "./03/ChildComponent";
-import BooleanComponent from "./03/BooleanComponent";
+import ChildComponent2 from "./03/ChildComponent2";
+
 
 class App extends Component {
     render() {
         return (
             <div>
-                <div><b>지루할 때 : </b><BooleanComponent bored /></div>
-                <div><b>지루하지 않을 때 : </b><BooleanComponent /></div>
+                <ChildComponent2 objValue={{age: '20살'}} requiredStringValue={'hi'}/>
             </div>
         );
     }
