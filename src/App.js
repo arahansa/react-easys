559b6c4f3eb1af4aedc7c14ad18c59d9dfc0ff6e
arahansa <arahansa@naver.com>
2020-06-25 17:08:14 +0900

    144 page 전

--- src/App.js
+++ src/App.js
@@ -4,29 +4,18 @@ import ForceUpdateExample from "./03/ForceUpdateExample";
 import LifecycleExample from "./03/LifecycleExample";
 import Counter from "./03/Counter";
 import NewCounter from "./03/NewCounter";
+import ListExample from "./03/ListExample";
 
 
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = {count : 10};
-        this.resetCount = this.resetCount.bind(this);
-    }
-
-    resetCount() {
-        this.setState( ({count}) => ({count: count + 10}));
-    }
-
-    componentDidMount() {
-        this.setState({hasDestroyed : true});
     }
 
     render() {
         return (
             <div>
-                <div><Counter count={this.state.count}></Counter></div>
-                <div><NewCounter count={this.state.count}/></div>
-                <button onClick={this.resetCount}>{this.state.count + 10} 으로 초기화</button>
+                <ListExample/>
             </div>
         );
     }
