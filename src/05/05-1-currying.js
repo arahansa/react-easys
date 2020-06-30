79917a55760f3bcfa2dc0a0ecdedc38254884f32
arahansa <arahansa@naver.com>
2020-06-30 17:28:08 +0900

    add currying

---
+++ src/05/05-1-currying.js
@@ -0,0 +1,18 @@
+
+
+function multiply(a,b){
+    return a*b;
+}
+
+function multiplyTwo(a){
+    return multiply(a,2);
+}
+
+function multiplyX(x){
+    return function(a){
+        return multiply(a,x);
+    }
+}
+
+const multiplyXArrow = x => a => multiply(a,x);
+
