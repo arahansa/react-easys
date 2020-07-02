8bdfdb71020c9d5d368bbdb9b6e14988f8f3ae81
pc-098\Administrator <arahansa@naver.com>
2020-07-02 20:44:00 +0900

    281p 까지 완료

--- src/06/HomePageComponent.jsx
+++ src/06/HomePageComponent.jsx
@@ -1,64 +1,62 @@
-import React, {PureComponent} from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-
-import Button from '../04/Button';
 import ButtonWithContext from './ButtonWithContext';
+import Button from '../04/Button';
 
-function RowBComponent(){
+function RowBComponent() {
   return <Button>버튼</Button>;
 }
 
-function RowCComponent(){
+function RowCComponent() {
   return <ButtonWithContext>버튼</ButtonWithContext>;
 }
 
-function TableComponent(){
+function TableComponent() {
   return (
     <table>
-      <RowBComponent/>
-      <RowCComponent/>
+      <RowBComponent />
+      <RowCComponent />
     </table>
-  )
+  );
 }
 
-class HomePageComponent extends PureComponent{
+class HomePageComponent extends PureComponent {
   constructor(props) {
     super(props);
 
-    this.state = {loading: false};
+    this.state = { loading: false };
     this.setLoading = this.setLoading.bind(this);
     this.toggleLoading = this.toggleLoading.bind(this);
   }
 
-  getChildContext(){
+  getChildContext() {
     return {
       loading: this.state.loading,
-      setLoading: this.setLoading
-    }
+      setLoading: this.setLoading,
+    };
   }
 
-  setLoading(loading){
-    this.setState({loading})
+  setLoading(loading) {
+    this.setState({ loading });
   }
 
-  toggleLoading(){
-    this.setState( ({loading}) => ({loading: !loading}));
+  toggleLoading() {
+    this.setState(({ loading }) => ({ loading: !loading }));
   }
 
   render() {
     return (
       <div>
-        <TableComponent/>
-        <Button onPress={this.toggleLoading}>상태변경</Button>
+        <TableComponent />
+        <Button onPress={this.toggleLoading}>상태 변경</Button>
       </div>
     );
   }
-
 }
 
 HomePageComponent.childContextTypes = {
   loading: PropTypes.bool,
-  setLoading: PropTypes.func
-}
+  setLoading: PropTypes.func,
+};
 
 export default HomePageComponent;
